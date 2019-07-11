0fc3c35d87511483fe4e6bf5c3949b9a6fa3baa6
joshuadl12 <joshuadeleon210@gmail.com>
2019-07-11 10:40:41 -0500

    NEW / UPDATED

---
+++ ts/module.ts
@@ -0,0 +1,8 @@
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@NgModule({
+  ...
+  imports: [MatButtonModule, MatCheckboxModule],
+  ...
+})
+export class PizzaPartyAppModule { }
---
+++ ts/start.ts
@@ -0,0 +1,6 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [BrowserAnimationsModule],
+})
+export class PizzaPartyAppModule {}
