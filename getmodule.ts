f0085c063360e4160cf0c85c3887e2b5118ea33e
joshuadl12 <joshuadeleon210@gmail.com>
2019-07-12 22:32:27 -0500

    NEW / UPDATED

--- getmodule.ts
+++ getmodule.ts
@@ -1,8 +1,7 @@
-import {MatButtonModule, MatCheckboxModule, MatNavList, MatToolbarModule, MatToolbar, MatSlider} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatNavList, MatToolbarModule, MatToolbar, MatSlider } from '@angular/material';
 import { NgModule } from '@angular/core';
-
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatNavList, MatToolbarModule, MatToolbar, MatToolbarModule, MatSlider],
   exports: [MatButtonModule, MatCheckboxModule, MatNavList, MatToolbarModule, MatToolbar, MatToolbarModule, MatSlider],
 })
-export class GetModule { }
+export class MyOwnCustomMaterialModule {}
--- icons.ts
+++ icons.ts
@@ -4,8 +4,10 @@ import {Component} from '@angular/core';
  * @title Basic icons
  */
 @Component({
+// tslint:disable-next-line: component-selector
   selector: 'icon-overview-example',
   templateUrl: 'icon-overview-example.html',
   styleUrls: ['icon-overview-example.css'],
 })
+// tslint:disable-next-line: component-class-suffix
 export class IconOverviewExample {}
